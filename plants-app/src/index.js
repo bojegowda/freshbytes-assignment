f4531d755e6078b5f656d911ed4b8fe6b6cdad7f
bojegowda671@gmail.com <bojegowda671@gmail.com>
2021-03-14 19:10:02 +0530

    store provided to add

--- plants-app/src/index.js
+++ plants-app/src/index.js
@@ -1,22 +1,13 @@
 import React from "react";
-import {
-  Provider
-} from "react-redux";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import store from "./store";
 
-ReactDOM.render( <
-  Provider store = {
-    store
-  } >
-  <
-  React.StrictMode >
-  <
-  App / >
-  <
-  /React.StrictMode>{" "} <
-  /Provider>,
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
