0d585eb0e6cd7597689fa4263d880dec0bec791b
bojegowda671@gmail.com <bojegowda671@gmail.com>
2021-03-14 10:27:36 +0530

    remover unused file

--- plants-app/src/index.js
+++ plants-app/src/index.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Provider } from "react-redux";
+import {
+  Provider
+} from "react-redux";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import store from "./store";
-import plantsStore from "./plantsStore";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>{" "}
-  </Provider>,
+ReactDOM.render( <
+  Provider store = {
+    store
+  } >
+  <
+  React.StrictMode >
+  <
+  App / >
+  <
+  /React.StrictMode>{" "} <
+  /Provider>,
   document.getElementById("root")
 );
